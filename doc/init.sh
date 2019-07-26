cc001434fc266785fd39ab8e0272e8479e60f01d
jonnyhtmso <860003213@qq.com>
2019-07-26 14:47:33 +0000

    jupyter

--- doc/init.sh
+++ doc/init.sh
@@ -1,7 +1,7 @@
-git config --global user.name "jonny zhong"
-git config --global user.email "860003213@qq.com"
-git init
-git remote add origin git@code.aliyun.com:jonnyfulcc/learning.git
-git add .
-git commit
-git push -u origin master
+mkdir -p /home/jovyan/home && cd /home/jovyan/home/ && \
+	git init && \
+	git pull https://github.com/jonnyhtmso/home.git && \
+git config --global user.name "jonnyhtmso" && \
+git config --global user.email "860003213@qq.com" &&\
+#git init && \
+git remote add origin https://github.com/jonnyhtmso/home.git 
