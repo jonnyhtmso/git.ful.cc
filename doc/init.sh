b9dc442a165ff3465da01894da02d2346ceb1d06
jonny zhong <860003213@qq.com>
2019-07-26 13:37:43 +0000

    jupyter

---
+++ doc/init.sh
@@ -0,0 +1,7 @@
+git config --global user.name "jonny zhong"
+git config --global user.email "860003213@qq.com"
+git init
+git remote add origin git@code.aliyun.com:jonnyfulcc/learning.git
+git add .
+git commit
+git push -u origin master
