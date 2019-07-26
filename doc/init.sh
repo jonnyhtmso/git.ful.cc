53b2575a52afc6f4cb63d53e418bd2fe14a80b02
jonnyhtmso <860003213@qq.com>
2019-07-26 23:16:11 +0800

    Update init.sh

--- doc/init.sh
+++ doc/init.sh
@@ -1,7 +1,9 @@
-mkdir -p /home/jovyan/home && cd /home/jovyan/home/ && \
-	git init && \
-	git pull https://github.com/jonnyhtmso/home.git && \
-git config --global user.name "jonnyhtmso" && \
-git config --global user.email "860003213@qq.com" &&\
+#mkdir -p /home/jovyan/home && cd /home/jovyan/home/ && \
+#	git init && \
+#	git pull https://github.com/jonnyhtmso/home.git && \
+#git config --global user.name "jonnyhtmso" && \
+#git config --global user.email "860003213@qq.com" &&\
 #git init && \
-git remote add origin https://github.com/jonnyhtmso/home.git 
+#git remote add origin https://github.com/jonnyhtmso/home.git 
+cp /home/jovyan/home/.ssh /home/jovyan/ && \
+echo "1q2w3e4r" | sudo -S service cron start
