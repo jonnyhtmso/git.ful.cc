8b64c78f43b81e58835d66f774aa7a26aa645622
jonnyhtmso <860003213@qq.com>
2019-11-03 15:08:30 +0000

    继续修改python_webdav模块,仍未改完wq

--- webdavtest/jianguoyun.py
+++ webdavtest/jianguoyun.py
@@ -1,7 +1,18 @@
 from python_webdav import client
+from urllib.parse import unquote
 
-clt = client.Client('https://dav.jianguoyun.com/','/dav')
+clt = client.Client('https://dav.jianguoyun.com/','/dav/%e6%88%91%e7%9a%84%e5%9d%9a%e6%9e%9c%e4%ba%91/')
 clt.set_connection(username='860003213@qq.com',password='yysyz0412')
 
 ls = list(filter(lambda x:not x.startswith('_'),dir(clt)))
 print(ls)
+clt.mkdir('/dav/%e6%88%91%e7%9a%84%e5%9d%9a%e6%9e%9c%e4%ba%91/wistron') #新建文件夹的时候需要使用完整路径
+clt.chdir('wistron')
+clt.upload_file('/home/jovyan/home/webdavtest/jianguoyun.py')
+lsfold = clt.ls()
+print(lsfold)
+pwd = clt.pwd()
+print(pwd)
+
+lsfold2 = [unquote(i[0]) for i in lsfold]
+print(lsfold2)
